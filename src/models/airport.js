b44ca3cb81b2a8726e36d9bd277ae73dbf626485
Aashish Joshi <theeaashish@gmail.com>
2025-08-25 08:16:40 +0000

    added flights model

--- src/models/airport.js
+++ src/models/airport.js
@@ -8,7 +8,20 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.belongsTo(models.City, {
+        foreignKey: "cityId",
+        onDelete: "CASCADE",
+      });
+
+      this.hasMany(models.Flight, {
+        foreignKey: "departureAirportId",
+        onDelete: "CASCADE",
+      });
+
+      this.hasMany(models.Flight, {
+        foreignKey: "arrivalAirportId",
+        onDelete: "CASCADE",
+      });
     }
   }
   Airport.init(
