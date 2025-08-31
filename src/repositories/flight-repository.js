2524bbf0f0bc9f89273784287561a8456700e912
Aashish Joshi <theeaashish@gmail.com>
2025-08-31 10:40:08 +0000

    feat: add joins in the flight-repository

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,5 +1,6 @@
+const { Sequelize } = require("sequelize");
 const CrudRepository = require("./crud-repositories");
-const { Flight } = require("../models");
+const { Flight, Airplane, Airport, City } = require("../models");
 
 class FlightRepository extends CrudRepository {
   constructor() {
@@ -10,6 +11,45 @@ class FlightRepository extends CrudRepository {
     const response = await Flight.findAll({
       where: filter,
       order: sort,
+      include: [
+        {
+          model: Airplane,
+          required: true,
+          as: "airplaneDetails",
+        },
+        {
+          model: Airport,
+          required: true,
+          on: {
+            col1: Sequelize.where(
+              Sequelize.col("Flight.departureAirportId"),
+              "=",
+              Sequelize.col("departureAirport.code")
+            ),
+          },
+          as: "departureAirport",
+          include: {
+            model: City,
+            required: true,
+          },
+        },
+        {
+          model: Airport,
+          required: true,
+          on: {
+            col1: Sequelize.where(
+              Sequelize.col("Flight.arrivalAirportId"),
+              "=",
+              Sequelize.col("arrivalAirport.code")
+            ),
+          },
+          as: "arrivalAirport",
+          include: {
+            model: City,
+            required: true,
+          },
+        },
+      ],
     });
     return response;
   }
